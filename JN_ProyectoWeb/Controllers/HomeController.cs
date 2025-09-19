46cd8e8ecf815720d0c1e20acfbd1fc3606f781f
DanielMoraa <danielmorasolano@gmail.com>
2025-09-18 23:56:15 -0600

    Parte UI - Progra Avanzada Web

---
+++ JN_ProyectoWeb/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_ProyectoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+
+    }
+}
