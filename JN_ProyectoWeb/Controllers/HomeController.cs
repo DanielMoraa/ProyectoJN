4c28f549793aa594495af48dda05db920ac5cbbc
DanielMoraa <danielmorasolano@gmail.com>
2025-10-02 20:48:40 -0600

    Semana 3 - Dapper y Pantalla de Registro

--- JN_ProyectoWeb/Controllers/HomeController.cs
+++ JN_ProyectoWeb/Controllers/HomeController.cs
@@ -5,6 +5,14 @@ namespace JN_ProyectoWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHttpClientFactory _http;
+        private readonly IConfiguration _configuration;
+        public HomeController(IHttpClientFactory http, IConfiguration configuration)
+        {
+            _http = http;
+            _configuration = configuration;
+        }
+
         #region Actions de Iniciar Sesión
 
         [HttpGet]
@@ -34,9 +42,22 @@ namespace JN_ProyectoWeb.Controllers
         [HttpPost]
         public IActionResult Registro(UsuarioModel usuario)
         {
-            //ToDo: registrar la información del usuario en la BD
+            using (var context = _http.CreateClient())
+            {
+                var urlApi = _configuration["Valores:UrlAPI"] + "Home/Registro";
+                var respuesta = context.PostAsJsonAsync(urlApi, usuario).Result;
 
-            return View();
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    var datosApi = respuesta.Content.ReadFromJsonAsync<int>().Result;
+
+                    if (datosApi > 0)
+                        return RedirectToAction("Index", "Home");
+                }
+
+                ViewBag.Mensaje = "No se ha registrado la información";
+                return View();
+            }
         }
 
         #endregion
