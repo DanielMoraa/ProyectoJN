4673ef962cff338c4c8ac4057aac0e35fc9bb97e
DanielMoraa <danielmorasolano@gmail.com>
2025-11-20 18:54:06 -0600

    Semana 10

--- JN_ProyectoAPI/Controllers/ProductoController.cs
+++ JN_ProyectoAPI/Controllers/ProductoController.cs
@@ -20,11 +20,12 @@ namespace JN_ProyectoAPI.Controllers
 
         [HttpGet]
         [Route("ConsultarProductos")]
-        public IActionResult ConsultarProductos()
+        public IActionResult ConsultarProductos(int ConsecutivoProducto)
         {
             using (var context = new SqlConnection(_configuration["ConnectionStrings:BDConnection"]))
             {
                 var parametros = new DynamicParameters();
+                parametros.Add("@ConsecutivoProducto", ConsecutivoProducto);
 
                 var resultado = context.Query<DatosProductoResponseModel>("ConsultarProductos", parametros);
                 return Ok(resultado);
@@ -48,6 +49,26 @@ namespace JN_ProyectoAPI.Controllers
                 return Ok(resultado!.ConsecutivoProducto);
             }
         }
+
+
+        [HttpPut]
+        [Route("ActualizarProductos")]
+        public IActionResult ActualizarProductos(ActualizarProductoRequestModel producto)
+        {
+            using (var context = new SqlConnection(_configuration["ConnectionStrings:BDConnection"]))
+            {
+                var parametros = new DynamicParameters();
+                parametros.Add("@ConsecutivoProducto", producto.ConsecutivoProducto);
+                parametros.Add("@Nombre", producto.Nombre);
+                parametros.Add("@Descripcion", producto.Descripcion);
+                parametros.Add("@Precio", producto.Precio);
+                parametros.Add("@Imagen", producto.Imagen);
+
+                var resultado = context.Execute("ActualizarProducto", parametros);
+                return Ok(resultado);
+            }
+        }
+        
 
     }
 }
